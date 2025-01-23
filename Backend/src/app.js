147ebc461f50aefb848fa4ea2c2b9b061ad0673e
Lakshya <lakshyasharma262001@gmail.com>
2025-01-23 19:26:19 +0530

    test

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -18,6 +18,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
 import contactusRouter from "./routes/contactus.routes.js";
 
 app.use("/api", contactusRouter);
